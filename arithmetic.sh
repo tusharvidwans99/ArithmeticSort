2f6448a1210aaf6b4a0a5221319576c45682d950
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 23:45:02 +0530

    Fourth operation is added

--- arithmetic.sh
+++ arithmetic.sh
@@ -7,3 +7,4 @@ read -p "Enter first number: " c
 first_operation=`echo $a+$b*$c | bc -l | awk '{printf "%.2f",$1}'`
 sec_operation=`echo $a*$b+$c | bc -l | awk '{printf "%.2f",$1}'`
 third_operation=`echo $c+$a/$b | bc -l | awk '{printf "%.2f",$1}'`
+fourth_operation=`echo $a%$b+$c | bc -l | awk '{printf "%.2f",$1}'`
