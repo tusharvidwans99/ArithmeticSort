879a6470158281eda8dccf5598a28e0c20628b2c
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-12 00:02:02 +0530

    read all the values of dict to an array

--- arithmetic.sh
+++ arithmetic.sh
@@ -11,3 +11,8 @@ Operations[third_operation]=`echo $c+$a/$b | bc -l | awk '{printf "%.2f",$1}'`
 Operations[fourth_operation]=`echo $a%$b+$c | bc -l | awk '{printf "%.2f",$1}'`
 
 echo ${Operations[@]}
+
+Operation_values[$i]=${Operations[first_operation]}
+Operation_values[$i]=${Operations[sec_operation]}
+Operation_values[$i]=${Operations[third_operation]}
+Operation_values[$i]=${Operations[fourth_operation]}
