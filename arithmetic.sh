7a4b8b0ab38c379271700e869b475843aeaa3942
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 23:33:38 +0530

    second operation added with correction

--- arithmetic.sh
+++ arithmetic.sh
@@ -5,4 +5,4 @@ read -p "Enter first number: " b
 read -p "Enter first number: " c
 
 first_operation=`echo $a+$b*$c | bc -l | awk '{printf "%.2f",$1}'`
-sec_operation=`echo $a*$b+$c | bc -l | awk '{printf "%.2f,$1"}'`
+sec_operation=`echo $a*$b+$c | bc -l | awk '{printf "%.2f",$1}'`
