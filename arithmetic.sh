c17bbde56e5d25186719d133d5a68f4fb5850924
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-11 22:07:25 +0530

    Second Operation is Added

--- arithmetic.sh
+++ arithmetic.sh
@@ -4,4 +4,5 @@ read -p "Enter first number: " a
 read -p "Enter first number: " b
 read -p "Enter first number: " c
 
-result=`echo $a+$b*$c | bc -l | awk '{printf "%.2f",$1}'`
+first_operation=`echo $a+$b*$c | bc -l | awk '{printf "%.2f",$1}'`
+sec_operation=`echo $a*$b+$c | bc -l | awk '{printf "%.2f,$1"}'`
