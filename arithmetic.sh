aa702eb27843c74c0b874cb491c293f489cef129
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-12 01:03:57 +0530

    Sorting the array in descending order

--- arithmetic.sh
+++ arithmetic.sh
@@ -10,9 +10,29 @@ Operations[sec_operation]=`echo $a*$b+$c | bc -l | awk '{printf "%.2f",$1}'`
 Operations[third_operation]=`echo $c+$a/$b | bc -l | awk '{printf "%.2f",$1}'`
 Operations[fourth_operation]=`echo $a%$b+$c | bc -l | awk '{printf "%.2f",$1}'`
 
-echo ${Operations[@]}
+i=0
+Operation_values[i++]=${Operations[first_operation]}
+Operation_values[i++]=${Operations[sec_operation]}
+Operation_values[i++]=${Operations[third_operation]}
+Operation_values[i++]=${Operations[fourth_operation]}
 
-Operation_values[$i]=${Operations[first_operation]}
-Operation_values[$i]=${Operations[sec_operation]}
-Operation_values[$i]=${Operations[third_operation]}
-Operation_values[$i]=${Operations[fourth_operation]}
+#sorting the array in descending order
+
+len=${#Operation_values[@]}
+for ((i=0; i<len; i++))
+do
+	for((j=0; j<len-i-1; j++))
+	do
+
+		if [ ${Operation_values[$j]} -lt ${Operation_values[$(($j+1))]} ]
+		then
+			temp=${Operation_values[$j]}
+			Operation_values[$j]=${Operation_values[$(($j+1))]}
+			Operation_values[$(($j+1))]=$temp
+		fi
+	done
+done
+echo ${Operation_values[*]}
+
+
+echo ${!Operation_values[@]}
